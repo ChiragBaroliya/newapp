0e4d9339da9cb25f8a94e1a74c3dfba61cc2e1a3
Chirag Baroliya <chiragsuccess07@gmail.com>
2023-02-12 13:45:52 +0530

    Implement Pagination

--- src/components/News.js
+++ src/components/News.js
@@ -11,14 +11,39 @@ import NewsItem from './NewsItem'
      }
 
      async componentDidMount(){
-      let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ce3325100b7048659e7c552185c36483";
+      let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ce3325100b7048659e7c552185c36483&page=1&&pagesize=20";
       let data = await fetch(url);
       let parsedData= await data.json();
-      this.setState({articles : parsedData.articles})
+      this.setState({articles : parsedData.articles, page:1, totalResults : parsedData.totalResults})
+     }
+
+      handlePreviousClick = async() =>{
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ce3325100b7048659e7c552185c36483&page=${this.state.page - 1}&pagesize=20`;
+        let data = await fetch(url);
+        let parsedData= await data.json();
+        this.setState({
+          articles : parsedData.articles,
+          page:this.state.page - 1
+        })
+     }
+
+     handleNextClick =async () =>{
+      if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)){
+
+      }   
+      else {
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ce3325100b7048659e7c552185c36483&page=${this.state.page + 1}&pagesize=20`;
+        let data = await fetch(url);
+        let parsedData= await data.json();
+        this.setState({
+          articles : parsedData.articles,
+          page:this.state.page + 1
+        })
+      } 
+      
      }
 
     render() {
-      console.log("render")
       return (
         <div className='container my-3'>
           <h2>NewsMoneky - Top Headlines</h2>
@@ -34,6 +59,10 @@ import NewsItem from './NewsItem'
             </div>
           })}
           
+          </div>
+          <div className='container d-flex justify-content-between'>
+            <button disabled={this.state.page <= 1} type='button' className='btn btn-dark' onClick={this.handlePreviousClick}> &larr; Previous</button>
+            <button type='button' className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
           </div>
         </div>
       )
